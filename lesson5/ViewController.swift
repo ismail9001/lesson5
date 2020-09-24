d83bc22bc9ad9c0689c2a4223da20cc93c5c23d0
Iskander Nizamov <nizamov-iskander@yandex.ru>
2020-09-24 14:32:19 +0300

    Initial Commit

---
+++ lesson5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson5
+//
+//  Created by macbook on 24.09.2020.
+//  Copyright © 2020 Iskander Nizamov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
